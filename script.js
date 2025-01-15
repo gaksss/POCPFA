7dc6a10e732cb005c8fc248d43f2d0c1a7c9a606
gaksss <clement.carlier2@gmail.com>
2025-01-15 22:13:56 +0100

    Setup

---
+++ script.js
@@ -0,0 +1,13 @@
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+map.on("click", function (e) {
+  var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+});
+
+
